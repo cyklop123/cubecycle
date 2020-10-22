d8ca5ba1056aad0cb8a79dbc8971480645ed8b3b
cyklop123 <irekbiela@gmail.com>
2020-10-23 00:20:32 +0200

    Add session authorization to socket.io

--- server.js
+++ server.js
@@ -12,24 +12,27 @@ const session = require('express-session')
 const mongoStore = require('connect-mongo')(session)
 const passport = require('passport')
 const methodOverride = require('method-override')
+const passportSocketIo = require('passport.socketio')
+const cookieParser = require('cookie-parser')
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set('view engine', 'ejs')
-app.use(expressLayouts)
 app.set('layout', './layouts/layout')
 app.set('layout room', 'false')
 
+app.use(expressLayouts)
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 app.use(flash());
+const sessionStore = new mongoStore({ url: process.env.DB_CONNECTION_STRING })
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: new mongoStore({ url: process.env.DB_CONNECTION_STRING })
+    store: sessionStore
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -40,6 +43,13 @@ const passportInit = require('./config/passport-config')(passport)
 const indexRoute = require('./routes/index');
 const { MongoStore } = require('connect-mongo');
 indexRoute(passport)
+io.use(passportSocketIo.authorize({
+    key: 'connect.sid',
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    passport: passport,
+    cookieParser: cookieParser
+}))
 
 /* Database connection */
 mongoose.connect(process.env.DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -48,10 +58,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to database'))
 
 app.use('/', indexRoute.router)
-
-io.use((socket, next)=>{
-    console.log(socket.request)
-})
 
 io.on('connection',(socket)=>{
     console.log('New connection',socket.id);
